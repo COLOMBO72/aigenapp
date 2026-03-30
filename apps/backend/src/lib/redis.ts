6be00253322bc9b89b121c5a2d8c3c558fea2e9b
COLOMBO72 <estwood94@mail.ru>
2026-03-30 17:35:03 +0700

    fix: redis connection via upstash

--- apps/backend/src/lib/redis.ts
+++ apps/backend/src/lib/redis.ts
@@ -2,7 +2,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const redisConnection = process.env.REDIS_URL
-  ? { url: process.env.REDIS_URL }
+  ? {
+      url: process.env.REDIS_URL,
+      tls: process.env.REDIS_URL.startsWith('rediss://') 
+        ? {} 
+        : undefined,
+    }
   : {
       host: process.env.REDIS_HOST || 'localhost',
       port: parseInt(process.env.REDIS_PORT || '6379'),
